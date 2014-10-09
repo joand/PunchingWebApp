9d502e34eb311c91ffde4e458fc678d63a0305da
Joel ANDRIAMANAMPISOA <joelandria@gmail.com>
2014-10-10 01:12:24 +0200

    Change project layout ...

--- View/src/main/java/fr/joand/pwa/view/servlets/DefaultServlet.java
+++ View/src/main/java/fr/joand/pwa/view/servlets/DefaultServlet.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package fr.joand.pwa.servlets;
+package fr.joand.pwa.view.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
